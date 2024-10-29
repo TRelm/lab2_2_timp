688bc2b130e5ecacbb8e83ffd681e15034b38439
TRelm <yagolnitskiy128@gmail.com>
2024-10-29 23:30:08 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cctype>
+#include "modAlphaCipher.h"
+#include <locale>
+using namespace std;
+void check(const wstring& Text, const int & key)
+{
+	wstring t=Text;
+	try {
+		modAlphaCipher cipher(key);
+		wcout<<cipher.encrypt(key, t)<<endl;
+		wstring en=cipher.encrypt(key, t);
+        wcout<<cipher.decrypt(key, en)<<endl;
+	} catch (const cipher_error & e) {
+		cerr<<"Error: "<<e.what()<<endl;
+	}
+}
+int main()
+{
+	locale loc("ru_RU.UTF-8");
+	locale::global(loc);
+	check(L"HELLO",2);
+	check(L"HelLo",5);
+	check(L"HELLO",0);
+	return 0;
+}

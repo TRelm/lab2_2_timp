4a8a240eb3b06e62c03a3241f3a4ebbc4606490d
TRelm <yagolnitskiy128@gmail.com>
2025-01-09 12:20:08 +0300

    l

--- modAlphaCipher.cpp
+++ modAlphaCipher.cpp
@@ -1,99 +1,110 @@
-#include "modAlphaCipher.h"
-modAlphaCipher::modAlphaCipher(int k)
+#include <Cipher.h>
+Cipher::Cipher(const std::wstring & ws_key)
 {
-	this->key=k;
+	std::wstring tmp(ws_key);
+	columns = getValidKey(tmp);
 }
-std::wstring modAlphaCipher::encrypt(modAlphaCipher key, std::wstring& open_text)
+void Cipher::set_key(std::wstring & ws_key)
 {
-	std::wstring Result;
-	const int str = ((open_text.size()-1)/key.key)+1;
-	int index = 0;
-	wchar_t mtr[str][key.key];
-	for (int i = 0; i <str; i++) {
-		for (int j = 0; j < key.key; j++ ) {
-			if (index < open_text.size()) {
-				mtr[i][j] = open_text[index];
-				index++;
+	columns = getValidKey(ws_key);
+}
+void Cipher::set_tableform(const std::wstring& open_text)
+{
+	len_text = open_text.size();
+	if (len_text>columns) {
+		rows = len_text/columns;
+		if (len_text%columns >0) rows+=1;
+	} else {
+		rows = 1;
+	}
+}
+std::wstring Cipher::encrypt(const std::wstring& open_text)
+{
+	std::wstring tmp(open_text);
+	tmp = getValidOpenText(tmp);
+	set_tableform(tmp);
+	std::wstring table_chiper[rows][columns];
+	int nums_text = 0;
+	for (int y=0; y<rows; y++) {
+		for (int x=0; x<columns; x++) {
+			if (nums_text < len_text) {
+				table_chiper[y][x] = tmp[nums_text];
 			} else {
-				mtr[i][j] =' ';
+				table_chiper[y][x] = L"-";
 			}
+			nums_text++;
 		}
 	}
-	for (int i = 0; i < key.key; i++) {
-		for (int j = 0; j < str; j++ ) {
-			Result.push_back(mtr[j][i]);
+	std::wstring encrypt_text;
+	for (int x=columns-1; x>=0; x--) {
+		for (int y=0; y<rows; y++) {
+			encrypt_text+=table_chiper[y][x];
 		}
 	}
-	return Result;
+	return encrypt_text;
 }
-std::wstring modAlphaCipher::decrypt(modAlphaCipher key, std::wstring& cipher_text)
+std::wstring Cipher::decrypt(const std::wstring& cipher_text)
 {
-	std::wstring Result;
-	int index = 0;
-	const int str = ((cipher_text.size()-1)/key.key)+1;
-	wchar_t mtr[str][key.key];
-	for (int i = 0; i < key.key; i++) {
-		for (int j = 0; j < str; j++ ) {
-			if (index < cipher_text.size()) {
-				mtr[j][i] = cipher_text[index];
-				index++;
-			} else {
-				mtr[j][i] = ' ';
-				index++;
-			}
+	set_tableform(getValidCipherText(cipher_text));
+	std::wstring table_chiper[rows][columns];
+	int nums_text = 0;
+	for (int x=columns-1; x>=0; x--) {
+		for (int y=0; y<rows; y++) {
+			table_chiper[y][x] = cipher_text[nums_text];
+			nums_text++;
 		}
 	}
-	for(int i = 0; i < str; i++) {
-		for (int j = 0; j< key.key; j++) {
-			Result.push_back(mtr[i][j]);
+	std::wstring decrypt_text;
+	for (int y=0; y<rows; y++) {
+		for (int x=0; x<columns; x++) {
+			if (table_chiper[y][x] != L"-")
+				decrypt_text+=table_chiper[y][x];
 		}
 	}
-	return Result;
+	return decrypt_text;
 }
-inline int modAlphaCipher::getValidKey(const int k, const std::wstring & s)
+inline int Cipher::getValidKey(std::wstring & ws_key)
 {
-	if (k<=0)
-		throw cipher_error("Invalid key");
-	else if (k>(s.size()/2))
-		throw cipher_error("Invalid key");
-	else
-		return k;
+	if (ws_key.empty())
+		throw cipher_error("Empty key");
+	std::string s_key = codec.to_bytes(ws_key);
+	for (auto & c:ws_key) {
+		if (!iswdigit(c)) {
+			throw cipher_error(std::string("Invalid key: ") + s_key);
+		}
+	}
+	int key = std::stoi(ws_key);
+	if (key<=0) {
+		throw cipher_error(std::string("Key must be greater than 0"));
+	}
+	return key;
 }
-inline std::wstring modAlphaCipher::getValidOpenText(const std::wstring & s)
+inline std::wstring Cipher::getValidOpenText(const std::wstring & ws_open_text)
 {
-	int i=0;
 	std::wstring tmp;
-	for (auto c:s) {
-		if (isalpha(c)) {
-			if (islower(c))                {
-				tmp.push_back(toupper(c));
-				i++;
-				if (i==1)
-					std::wcout<<L"Часть входного текста была перенесена в верхний регистр: "<<std::endl;
-			} else
+	for (auto c:ws_open_text) {
+		if (iswalpha(c)) {
+			if (iswlower(c))
+				tmp.push_back(towupper(c));
+			else
 				tmp.push_back(c);
 		}
 	}
 	if (tmp.empty())
-		throw cipher_error("No input text");
+		throw cipher_error("Empty open text");
 	return tmp;
 }
-inline std::wstring modAlphaCipher::getValidCipherText(const std::wstring & s)
+inline std::wstring Cipher::getValidCipherText(const std::wstring & ws_cipher_text)
 {
-	int i=0;
-	std::wstring tmp;
-	for (auto c:s) {
-		if (isalpha(c)) {
-			if (islower(c))                {
-				tmp.push_back(toupper(c));
-				i++;
-				if (i==1)
-					throw cipher_error("Invalid cipher text");
-			} else
-				tmp.push_back(c);
+	if (ws_cipher_text.empty())
+		throw cipher_error("Empty cipher text");
+
+	for (auto c:ws_cipher_text) {
+		if (!iswupper(c)) {
+			if (c!=L'-') {
+				throw cipher_error(std::string("Invalid cipher text"));
+			}
 		}
 	}
-	if (tmp.empty())
-		throw cipher_error("No output text");
-	return tmp;
+	return ws_cipher_text;
 }
